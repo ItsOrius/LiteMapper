f56b8971b772743ce57b2af2f9dec7ef5fbb0e51
Orius <32719997+ItsOrius@users.noreply.github.com>
2021-12-19 20:00:21 -0500

    lasers are now much slower

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ function calculate(_input) {
         beatmap._events.push({
             _time: note.raw._time,
             _type: laserSide == 2 ? 12 : 13,
-            _value: Math.ceil((Math.ceil((2 / note.padding) + 1) * Math.ceil((2 / note.padding) + 1)) / 2)
+            _value: Math.ceil((Math.ceil((2 / note.padding) + 1) ** 2) / 4)
         });
         beatmap._events.push({
             _time: note.raw._time,
