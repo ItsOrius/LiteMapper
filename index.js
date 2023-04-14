6238ff80a58ebe13b82d10a127d4764e10732faa
Orius <32719997+ItsOrius@users.noreply.github.com>
2023-04-14 02:36:53 +0000

    fixed urls

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+
+// redirect to index if no params
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// redirect everything in /public
+fs.readdirSync(__dirname + '/public').forEach(file => {
+  if (file.endsWith('.html') && file != "index.html") {
+    app.get('/' + file.replace('.html', ''), (req, res) => {
+      res.sendFile(__dirname + '/public/' + file);
+    });
+  }
+});
+
+// start express server
+app.listen(3000, () => {
+  console.log(`Started listening on port 3000.`);
+});
